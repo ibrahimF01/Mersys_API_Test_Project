f94c970e16a09095396b91578ff23da3e14077d6
omer-gurbuz <ofg4434@gmail.com>
2022-10-11 19:29:38 +0300

    omerTest

---
+++ src/test/java/Campus/SubjCategoriesTest.java
@@ -0,0 +1,50 @@
+package Campus;
+
+public class SubjCategoriesTest {
+
+        private String id = null;
+        private String name;
+        private String code;
+        private boolean active = true;
+        private String[] translateName = {};
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String[] getTranslateName() {
+        return translateName;
+    }
+
+    public void setTranslateName(String[] translateName) {
+        this.translateName = translateName;
+    }
+}
